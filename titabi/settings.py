33e46de3a4140ed1ae81196f7d9988dfc0f8ff13
localdevv <mosesogheneo@gmail.com>
2025-07-10 14:17:38 +0100

    Auth user

--- titabi/settings.py
+++ titabi/settings.py
@@ -141,4 +141,8 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
+
+AUTH_USER_MODEL = 'accounts.User'
