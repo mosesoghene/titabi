d59b6dc3b9220415c6d05602d9f25f91a79b3b76
localdevv <mosesogheneo@gmail.com>
2025-07-28 10:28:00 +0100

    Job admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from accounts.serilizers import UserSerializer
+from .models import JobRequest
+
 # Register your models here.
+admin.site.register(JobRequest, UserSerializer)
+class JobAdmin(admin.ModelAdmin):
+    fieldsets = (
+        'created_by', 'artisan', 'category',
+        'description', 'location', 'status',
+        'created_at', 'updated_at', 'target_artisan'
+    )
+
+    readonly_fields = ('created_at', 'updated_at')
+    list_display = (
+        'id', 'created_by', 'artisan', 'category',
+        'description', 'location', 'status',
+        'created_at', 'updated_at', 'target_artisan'
+    )
